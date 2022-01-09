cdfdf730727bd4555927ee8f797d608cb6c76e4f
Kai R <k@pekaru.de>
2022-01-09 11:22:48 +0100

    Trouble with ws file

--- MSP430-I2C-Slave/src/main.c
+++ MSP430-I2C-Slave/src/main.c
@@ -25,6 +25,16 @@
 #include <msp430.h>
 #include "msp430-i2c.h"
 
+//////////////////////////////////////////////////////////////////////////////
+/// Global definitions
+//////////////////////////////////////////////////////////////////////////////
+
+#define NUM_OF_ADCVALUES 12
+
+//////////////////////////////////////////////////////////////////////////////
+/// Module global variables
+//////////////////////////////////////////////////////////////////////////////
+
 //uint8_t slvData[NUMBER_OF_BYTES] = {'A','1','C','2'};   // Data buffer (extern in msp430-i2c.c) !!!
 
 //////////////////////////////////////////////////////////////////////////////
@@ -32,7 +42,14 @@
 //////////////////////////////////////////////////////////////////////////////
 
 void sd16Setup(void);
+uint16_t getMedian(uint16_t* data, uint8_t items); 
 
+//////////////////////////////////////////////////////////////////////////////
+/// @brief Mainprogram:  Reading of voltage data on the ADC and 
+///                      transmission of the measured values to an I2C master
+/// 
+/// @return int 
+//////////////////////////////////////////////////////////////////////////////
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;               // Stop watchdog
@@ -44,24 +61,41 @@ int main(void)
   DCOCTL = 0;                             // Select lowest DCOx and MODx settings
   BCSCTL1 = CALBC1_1MHZ;                  // Set DCO
   DCOCTL = CALDCO_1MHZ;
-    
+
+  uint16_t median;              
+  uint16_t adcValues[NUM_OF_ADCVALUES];   // Array with the data for which 
+                                          // the meriadian is to be found
+  uint8_t idx = 0;
+
   i2cSlaveSetup();
   sd16Setup();
-
   while(1) {
     SD16CCTL0 &= ~SD16IFG;                // clear ADC Interrupt Flag
-    SD16CCTL0 |= SD16SC;                  // Start ADC
+    SD16CCTL0 |= SD16SC;                  // AD Wandler starten
     while (!(SD16CCTL0 & SD16IFG)) { 
-    }                                     // ADC ready?
-    __disable_interrupt();
-    setTxData16(SD16MEM0);                // transmit ADC value
-    __enable_interrupt();
-    __delay_cycles(25000);
-  } 
+    }                                     // AD Wandlung fertig ?
+    adcValues[idx] = SD16MEM0;            // neuen Wert einsortieren - in
+                                          // das Array schreiben
+    idx++;
+    if (idx == NUM_OF_ADCVALUES)          // die Werte stehen auf Platz 0
+                                          // bis numofvalues-1
+    {
+      median = getMedian(adcValues, NUM_OF_ADCVALUES); //Median finden
+      __disable_interrupt();
+      setTxData16(median);                // ADC Wert übertragen
+      __enable_interrupt();
+      __delay_cycles(25000);
+      idx = 0;
+    }
+  }  
  // LPM0;                                   // CPU off, await USI interrupt
  // __no_operation();
 }
 
+//////////////////////////////////////////////////////////////////////////////
+/// @brief Set up ADC in single mode without interrupt
+/// 
+//////////////////////////////////////////////////////////////////////////////
 void sd16Setup() {
   SD16CTL = SD16REFON;      // Referenzvoltage µC, 1200 mV
   SD16CTL |= SD16SSEL_1;    // Clk is SMCLK
@@ -70,4 +104,38 @@ void sd16Setup() {
   SD16CCTL0 = SD16UNI;      // Unipolar Input 0 up to 0xFFFF
   SD16CCTL0 |= SD16SNGL;    // Single-Mode; 
   SD16CCTL0 |= SD16DIV_0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/// @brief Determining the median from a list of measured values
+/// 
+/// @param data           Measured values
+/// @param items          Number of measured values
+/// @return uint16_t      Return median value 
+//////////////////////////////////////////////////////////////////////////////
+uint16_t getMedian(uint16_t* data, uint8_t items)
+{ 
+  uint16_t tmp;
+  uint8_t i, j;
+
+  for (i = 0; i < items; ++i) 
+  {
+    for (j = 0; j < items - i - 1; ++j)
+    {
+      if (data[j] > data[j + 1])
+      {
+        tmp = data[j];
+        data[j] = data[j + 1];
+        data[j + 1] = tmp;
+      }
+    }
+  }
+  tmp = (items >> 1);                     // items/2
+#if (NUM_OF_ADCVALUES & 0x01)
+  return data[tmp];
+#else 
+  return ((data[tmp] + data[tmp-1]) >>1); // The data list has an even number of elements. 
+                                          // Therefore calculate the mean value from the 
+                                          // two mean data values. Fractions are rounded off.
+#endif
 }
